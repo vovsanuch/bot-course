70d6754936374c57e31b0445d66478cc71ffd9d1
vovsanuch <v.a.poleshchuk@yandex.ru>
2021-09-08 20:28:03 +0300

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const TelegramApi = require('node-telegram-bot-api')
+const {gameOptions, againOptions} = require('./options')
+const token = '1997245135:AAEY9Xqy0hgp8JKmzejRFO5E_sOCH0b3R40'
+
+const bot = new TelegramApi(token, {polling: true})
+
+const chats = {}
+
+
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, 'Сейчас я загадываю цифру от 0 до 9, а ты должен ее угадать!')
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, 'Отгадай', gameOptions)
+}
+
+
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Начальное приветствие'},
+        {command: '/info', description: 'Получить информацию о пользователе'},
+        {command: '/game', description: 'Игра угадай цифру'}
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+
+        if (text === '/start'){
+            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/2.webp')
+            return bot.sendMessage(chatId, 'Добро пожаловать в телеграм-бот')
+        }
+
+        if (text === '/info'){
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`)
+        }
+        if (text === '/game') {
+            return startGame(chatId)
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!')
+    })
+
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+        if (data == chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions)
+        } else {
+            return  bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions)
+        }
+    })
+}
+
+start()
